8e2182af6083bc3604ccc91eca9b616707a447dd
r.lopez <r.lopez@htime.org>
2026-08-20 08:53:52 +0200

    Create logging messages

--- src/run_experiment.py
+++ src/run_experiment.py
@@ -9,4 +9,8 @@ from src.config.config import Config
 if __name__ == "__main__":
     config = Config()
     experiment = Experiment(config)
+    print('Running Experiment ...')
+    print('\n')
     experiment.run()
+    print('\n')
+    print('Experiment Finished!!')
